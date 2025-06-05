e4565be18143c044bbe8cc9bb5108b7b8c360461
Christian Kissig <dev@kissig.fastmail.fm>
2025-06-05 17:18:28 +0100

    adding package exports for document, client handler, and process wrapper

--- isabelle_lsp_client/__init__.py
+++ isabelle_lsp_client/__init__.py
@@ -1,7 +1,13 @@
 from .client import IsabelleClient
+from .handler import ClientHandler
+from .document import Document
+from .process import IsabelleProcess
 from .version import version as __version__
 
 __all__ = [
         "__version__",
         "IsabelleClient",
+        "ClientHandler",
+        "Document",
+        "IsabelleProcess",
 ]
